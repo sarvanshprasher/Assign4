cc3f80e4b76b66aa6573dd4c843304b31c78fc12
Sarvansh Prasher <sarvanshp@gmail.com>
2020-02-25 21:58:53 -0700

    Added files relevant to assignment and added changes in main storyboard

---
+++ Assign4sprasher/Model/PlaceDescription.swift
@@ -0,0 +1,9 @@
+//
+//  PlaceDescription.swift
+//  Assign4sprasher
+//
+//  Created by sarvansh prasher on 2/25/20.
+//  Copyright © 2020 Sarvansh prasher. All rights reserved.
+//
+
+import Foundation
